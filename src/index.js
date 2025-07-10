b7e5b4d5119ad4b84a4bc3502ba57a3fec20a98e
chu quang tu <chuquangtus2004@gmail.com>
2025-07-10 23:40:14 +0700

    MVC Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { engine } from "express-handlebars";
-const app = express();
 import path from "path";
+import morgan from "morgan";
+import route from "./routes/index.js";
+const app = express();
 const __dirname = path.resolve();
+// app.use(morgan("combined"));
 
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
@@ -10,12 +13,9 @@ app.set("views", "./src/resources/views");
 
 app.use(express.static(path.join(__dirname, "src", "public")));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-app.get("/views", (req, res) => {
-  res.render("views");
-});
+//Routes init
+route(app);
+
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
