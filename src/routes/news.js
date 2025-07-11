5aabd2984dc4bac476d77cb57a771310e8e72b43
chu quang tu <chuquangtus2004@gmail.com>
2025-07-11 11:46:18 +0700

    instail prettier lint-staged husky

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,8 +1,8 @@
-import express from "express";
-import newsController from "../app/controllers/NewsController.js";
+import express from 'express';
+import newsController from '../app/controllers/NewsController.js';
 const router = express.Router();
 
-router.use("/:slug", newsController.show);
-router.use("/", newsController.index);
+router.use('/:slug', newsController.show);
+router.use('/', newsController.index);
 
 export default router;
