83f17d0552ecda219f1c6e1e26d29334d27bfbab
chu quang tu <chuquangtus2004@gmail.com>
2025-07-17 18:11:20 +0700

    delete-count

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -2,10 +2,20 @@ import Course from '../models/Course.js';
 import mongoose from '../../util/mongoose.js';
 
 class MeController {
-    async storedController(req, res) {
+    async storedController(req, res, next) {
         try {
-            const courses = await Course.find({});
+            const CoursesDeletedPromise = Course.countDocumentsWithDeleted({
+                deleted: true,
+            });
+            const coursesPromise = Course.find({});
+
+            const [countCoursesDeleted, courses] = await Promise.all([
+                CoursesDeletedPromise,
+                coursesPromise,
+            ]);
+
             res.render('me/stored-courses', {
+                countCoursesDeleted,
                 courses: mongoose.multipleMongooseToObject(courses),
             });
         } catch (error) {
